565924c8ad485534d72af72e3052c82cf86742c2
Martin <martinsanchez2533@gmail.com>
2024-06-06 10:16:44 -0300

    agrega estructura y estilo login/register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,14 +4,20 @@ import { AuthRoutingModule } from './auth-routing-module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
 @NgModule({
-  declarations: [LayoutPageComponent, RegisterPageComponent, LoginPageComponent],
+  declarations: [
+    LayoutPageComponent,
+    RegisterPageComponent,
+    LoginPageComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
